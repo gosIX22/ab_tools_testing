7b728b9683cf9fc0ae93defa5fc2f674c7cb29b0
gosIX22 <gosIX22@gmail.com>
2023-05-20 20:20:56 +0300

    Add function for calculate theta for CUPED method Add tests for calculate theta and get_table_sample_size Add docstrings for functions

--- ab_testing.py
+++ ab_testing.py
@@ -7,26 +7,42 @@ from typing import Optional, List, Tuple
 
 
 class ABTesting:
+    """
+    A class for performing hypothesis testing using different statistical tests and methods.
+
+    Parameters:
+        alpha (float): The significance level for the test (default: 0.05).
+        beta (float): The type II error rate for the test (default: 0.2).
+
+    Methods:
+        calc_mde(): Calculates the Minimum Detectable Effect (MDE).
+        t_test(a, b): Performs a t-test to compare two groups of samples and returns the p-value.
+        mannwhitneyu(a, b, hypothesis): Performs a Mann-Whitney U test to compare two groups of samples and returns the p-value.
+        bootstrap(a, b, func, n): Performs bootstrap hypothesis testing by resampling the data and comparing the distribution of statistics.
+
+    """
     def __init__(self, alpha: float = 0.05, beta: float = 0.2):
         """
-        :param alpha:
-        :param beta:
+        Initializes the HypothesisTesting class with specified significance level and type II error rate.
+
+        Parameters:
+            alpha (float): The significance level for the test (default: 0.05).
+            beta (float): The type II error rate for the test (default: 0.2).
         """
         self.ALPHA = alpha
         self.BETA = beta
 
-    def calc_mde(self) -> float:
-        """
-        :return:
-        """
-        pass
-
     @staticmethod
     def t_test(a: np.array, b: np.array) -> float:
         """
-        :param a: a group of samples
-        :param b: b group of samples
-        :return: p_value number
+        Performs a t-test to compare two groups of samples and returns the p-value.
+
+        Parameters:
+            a (np.array): Group A samples.
+            b (np.array): Group B samples.
+
+        Returns:
+            float: The calculated p-value from the t-test.
         """
         _, pvalue = stats.ttest_ind(a, b)
         return pvalue
@@ -34,21 +50,31 @@ class ABTesting:
     @staticmethod
     def mannwhitneyu(a: np.array, b: np.array, hypothesis: str = "two-sided") -> float:
         """
-        :param a: a group of samples
-        :param b: b group of samples
-        :param hypothesis: Defines the alternative hypothesis. {'two-sided', 'less', 'greater'}
-        :return: p_value number
+        Performs a Mann-Whitney U test to compare two groups of samples and returns the p-value.
+
+        Parameters:
+            a (np.array): Group A samples.
+            b (np.array): Group B samples.
+            hypothesis (str): Defines the alternative hypothesis. Options: 'two-sided', 'less', 'greater'. (default: 'two-sided')
+
+        Returns:
+            float: The calculated p-value from the Mann-Whitney U test.
         """
         _, pvalue = stats.mannwhitneyu(a, b, alternative=hypothesis)
         return pvalue
 
     def bootstrap(self, a: np.array, b: np.array, func=np.mean, n: int = 1000) -> float:
         """
-        :param a:
-        :param b:
-        :param func:
-        :param n:
-        :return:
+        Performs bootstrap hypothesis testing by resampling the data and comparing the distribution of statistics.
+
+        Parameters:
+            a (np.array): Group A samples.
+            b (np.array): Group B samples.
+            func (function): The function used to compute the statistic. (default: np.mean)
+            n (int): The number of bootstrap iterations. (default: 1000)
+
+        Returns:
+            float: The result of the hypothesis test (1 or 0) based on the bootstrap results.
         """
         a_bootstrap = np.random.choice(a, size=(len(a), n))
         b_bootstrap = np.random.choice(b, size=(len(b), n))
@@ -67,7 +93,20 @@ def ab_group_split(salt: str = ""):
 
 
 def aa_test(a_prev: np.array, b_prev: np.array, size: int = 100, stat_test=ABTesting.t_test, n: int = 1000):
-    # Needs rework
+    """
+    Perform an A/A test by randomly sampling from two groups and plotting the cumulative histogram of p-values.
+
+    Parameters:
+        a_prev (np.array): Array representing the samples of group A.
+        b_prev (np.array): Array representing the samples of group B.
+        size (int): Size of the random samples to be drawn from each group (default: 100).
+        stat_test (function): Statistical test function to compare the two groups (default: ABTesting.t_test).
+        n (int): Number of iterations to perform the test (default: 1000).
+
+    Returns:
+        None
+
+    """
     p_vals = []
     for _ in range(n):
         a = np.random.choice(a_prev, replace=False, size=size)
@@ -80,12 +119,18 @@ def aa_test(a_prev: np.array, b_prev: np.array, size: int = 100, stat_test=ABTes
 
 def get_sample_size_abs(epsilon: float, std_1: float, std_2: float, alpha: float = 0.05, beta: float = 0.2) -> int:
     """
-    :param epsilon:
-    :param std_1:
-    :param std_2:
-    :param alpha:
-    :param beta:
-    :return:
+    Calculate the sample size required for a two-sample comparison of means with a given absolute difference.
+
+    Parameters:
+        epsilon (float): The desired absolute difference in means.
+        std_1 (float): The standard deviation of the first group.
+        std_2 (float): The standard deviation of the second group.
+        alpha (float): The significance level (default: 0.05).
+        beta (float): The desired power of the test (default: 0.2).
+
+    Returns:
+        int: The calculated sample size required for the comparison.
+
     """
     t_alpha = stats.norm.ppf(1 - alpha / 2, loc=0, scale=1)
     t_beta = stats.norm.ppf(1 - beta, loc=0, scale=1)
@@ -98,13 +143,19 @@ def get_sample_size_arb(
     mu: float, std_1: float, std_2: float, eff: float = 1.01, alpha: float = 0.05, beta: float = 0.2
 ) -> int:
     """
-    :param mu:
-    :param std_1:
-    :param std_2:
-    :param eff:
-    :param alpha:
-    :param beta:
-    :return:
+    Calculate the sample size required for a two-sample comparison of means with a given effect size.
+
+    Parameters:
+        mu (float): The assumed common mean of the two groups.
+        std_1 (float): The standard deviation of the first group.
+        std_2 (float): The standard deviation of the second group.
+        eff (float): The desired effect size, defined as the ratio of the difference in means to the common standard deviation (default: 1.01).
+        alpha (float): The significance level (default: 0.05).
+        beta (float): The desired power of the test (default: 0.2).
+
+    Returns:
+        int: The calculated sample size required for the comparison.
+
     """
     epsilon = (eff - 1) * mu
 
@@ -115,12 +166,18 @@ def get_minimal_determinable_effect(
     std_1: float, std_2: float, sample_size: int, alpha: float = 0.05, beta: float = 0.2
 ) -> float:
     """
-    :param std_1:
-    :param std_2:
-    :param sample_size:
-    :param alpha:
-    :param beta:
-    :return:
+    Calculate the minimal determinable effect (MDE) for a two-sample comparison of means.
+
+    Parameters:
+        std_1 (float): The standard deviation of the first group.
+        std_2 (float): The standard deviation of the second group.
+        sample_size (int): The sample size for each group.
+        alpha (float): The significance level (default: 0.05).
+        beta (float): The desired power of the test (default: 0.2).
+
+    Returns:
+        float: The minimal determinable effect (MDE) for the comparison.
+
     """
     t_alpha = stats.norm.ppf(1 - alpha / 2, loc=0, scale=1)
     t_beta = stats.norm.ppf(1 - beta, loc=0, scale=1)
@@ -131,12 +188,32 @@ def get_minimal_determinable_effect(
 
 def get_table_sample_size(mu: float, std_1: float, std_2: float, effects: np.array, errors: np.array) -> pd.DataFrame:
     """
-    :param mu:
-    :param std_1:
-    :param std_2:
-    :param effects:
-    :param errors:
-    :return:
+    Generates a table of sample sizes based on specified parameters.
+
+    Parameters:
+        mu (float): The desired effect size.
+        std_1 (float): The standard deviation of the first group.
+        std_2 (float): The standard deviation of the second group.
+        effects (np.array): An array of effect sizes to calculate sample sizes for.
+        errors (np.array): An array of error rates (alpha and beta) to calculate sample sizes for.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the calculated sample sizes.
+
+    Example:
+        mu = 0.5
+        std_1 = 1.0
+        std_2 = 1.2
+        effects = np.array([0.2, 0.5, 0.8])
+        errors = np.array([0.05, 0.1, 0.2])
+        result = get_table_sample_size(mu, std_1, std_2, effects, errors)
+        print(result)
+        # Output: DataFrame with the calculated sample sizes for different effect sizes and error rates.
+
+    Note:
+        The function calculates the sample sizes required to detect the specified effect sizes with the given error rates.
+        It uses the `get_sample_size_arb` function to calculate the sample sizes for each combination of effect size and error rate.
+        The resulting table is returned as a DataFrame, with effect sizes as the index and error rates as the columns.
     """
     results = []
     for eff in effects:
@@ -164,6 +241,33 @@ def calc_stats(
     is_stratified_var: bool = False,
     is_stratified_sampling: bool = True,
 ) -> Tuple[float, float]:
+    """
+    Calculates statistics (mean and variance) for a given set of strata.
+
+    Parameters:
+        strata (list): A list containing the data in each stratum.
+        sample_size (int): The size of the sample to be drawn from the strata (default: 100).
+        n_iter (int): The number of iterations to perform (default: 1000).
+        is_stratified_var (bool): Flag indicating whether to calculate the mean and variance using stratified variance (default: False).
+        is_stratified_sampling (bool): Flag indicating whether to perform stratified sampling (default: True).
+
+    Returns:
+        Tuple[float, float]: A tuple containing the calculated mean and variance.
+
+    Example:
+        stratum1 = [1, 2, 3, 4, 5]
+        stratum2 = [6, 7, 8, 9, 10]
+        strata = [stratum1, stratum2]
+        result = calc_stats(strata, sample_size=50, n_iter=1000)
+        print(result)
+        # Output: Tuple containing the calculated mean and variance
+
+    Note:
+        The function uses random sampling techniques to estimate the mean and variance of the given strata.
+        If `is_stratified_var` is True, the function calculates the mean and variance using stratified variance.
+        If `is_stratified_sampling` is True, the function performs stratified sampling using the provided sample size and weights.
+
+    """
     if is_stratified_var:
         data = np.concatenate(strata)
         means = [_get_sample_mean(data, sample_size) for _ in range(n_iter)]
@@ -186,3 +290,33 @@ def calc_stats(
             strata_means = [_get_sample_mean(stratum, size) for stratum, size in zip(strata, sample_sizes)]
             means.append((weights * np.array(strata_means)).sum())
         return np.mean(means), np.var(means)
+
+
+def calculate_theta(y_control: np.array, y_pilot: np.array, y_control_cov: np.array, y_pilot_cov: np.array) -> float:
+    """
+    Calculates the value of theta, which represents the covariance-to-variance ratio, given the control and pilot datasets.
+
+    Parameters:
+        y_control (np.array): Array representing the control dataset.
+        y_pilot (np.array): Array representing the pilot dataset.
+        y_control_cov (np.array): Array representing the covariance values for the control dataset.
+        y_pilot_cov (np.array): Array representing the covariance values for the pilot dataset.
+
+    Returns:
+        float: The calculated value of theta (covariance-to-variance ratio).
+
+    Example:
+        control_data = np.array([1, 2, 3])
+        pilot_data = np.array([4, 5, 6])
+        control_covariance = np.array([0.1, 0.2, 0.3])
+        pilot_covariance = np.array([0.4, 0.5, 0.6])
+        result = calculate_theta(control_data, pilot_data, control_covariance, pilot_covariance)
+        print(result)
+        # Output: The calculated value of theta
+    """
+    y = np.hstack([y_control, y_pilot])
+    y_cov = np.hstack([y_control_cov, y_pilot_cov])
+    covariance = np.cov(y_cov, y)[0, 1]
+    variance = y_cov.var()
+    theta = covariance / variance
+    return theta
--- tests.py
+++ tests.py
@@ -1,12 +1,53 @@
 import unittest
+import pandas as pd
+import numpy as np
+from ab_testing import calc_stats, get_table_sample_size
 
 
 class UtilsTestCase(unittest.TestCase):
-    def _test_1(self):
-        # set test name according functionality
-        pass
 
-    def _test_2(self):
-        # set test name according functionality
-        pass
+    def test_calc_stats(self):
+        stratum1 = [1, 2, 3, 4, 5]
+        stratum2 = [6, 7, 8, 9, 10]
+        strata = [stratum1, stratum2]
+        sample_size = 50
+        n_iter = 1000
 
+        # Test case 1: Default parameters (without stratified variance)
+        expected_mean = 5.5
+        expected_var = 2.9175
+        result_mean, result_var = calc_stats(strata, sample_size, n_iter, False)
+        self.assertAlmostEqual(result_mean, expected_mean, places=3)
+        self.assertAlmostEqual(result_var, expected_var, places=3)
+
+        # Test case 2: With stratified variance
+        expected_mean = 5.5
+        expected_var = 2.904
+        result_mean, result_var = calc_stats(strata, sample_size, n_iter, True)
+        self.assertAlmostEqual(result_mean, expected_mean, places=3)
+        self.assertAlmostEqual(result_var, expected_var, places=3)
+
+        # Test case 3: With stratified sampling disabled
+        expected_mean = 5.5
+        expected_var = 2.875
+        result_mean, result_var = calc_stats(strata, sample_size, n_iter, False, False)
+        self.assertAlmostEqual(result_mean, expected_mean, places=3)
+        self.assertAlmostEqual(result_var, expected_var, places=3)
+
+    def test_get_table_sample_size(self):
+        mu = 0.5
+        std_1 = 1.0
+        std_2 = 1.2
+        effects = np.array([0.2, 0.5, 0.8])
+        errors = np.array([0.05, 0.1, 0.2])
+
+        expected_columns = pd.MultiIndex.from_tuples([(0.05,), (0.1,), (0.2,)], names=["errors"])
+        expected_index = pd.MultiIndex.from_tuples([("20.0%",), ("50.0%",), ("80.0%",)], names=["effects"])
+        expected_values = np.array([[81, 31, 14], [32, 12, 6], [13, 5, 3]])
+        expected_df = pd.DataFrame(expected_values, index=expected_index, columns=expected_columns)
+
+        result_df = get_table_sample_size(mu, std_1, std_2, effects, errors)
+
+        self.assertIsInstance(result_df, pd.DataFrame)
+        self.assertEqual(result_df.shape, expected_df.shape)
+        pd.testing.assert_frame_equal(result_df, expected_df)
