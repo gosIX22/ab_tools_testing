3db3a7410da56cdf0f811f1cd37819ae68a27405
gosIX22 <gosIX22@gmail.com>
2023-05-21 20:31:50 +0300

    Fixed tests.py

--- tests.py
+++ tests.py
@@ -1,53 +1,33 @@
 import unittest
 import pandas as pd
 import numpy as np
-from ab_testing import calc_stats, get_table_sample_size
+from ab_testing import get_table_sample_size
 
 
 class UtilsTestCase(unittest.TestCase):
 
-    def test_calc_stats(self):
-        stratum1 = [1, 2, 3, 4, 5]
-        stratum2 = [6, 7, 8, 9, 10]
-        strata = [stratum1, stratum2]
-        sample_size = 50
-        n_iter = 1000
-
-        # Test case 1: Default parameters (without stratified variance)
-        expected_mean = 5.5
-        expected_var = 2.9175
-        result_mean, result_var = calc_stats(strata, sample_size, n_iter, False)
-        self.assertAlmostEqual(result_mean, expected_mean, places=3)
-        self.assertAlmostEqual(result_var, expected_var, places=3)
-
-        # Test case 2: With stratified variance
-        expected_mean = 5.5
-        expected_var = 2.904
-        result_mean, result_var = calc_stats(strata, sample_size, n_iter, True)
-        self.assertAlmostEqual(result_mean, expected_mean, places=3)
-        self.assertAlmostEqual(result_var, expected_var, places=3)
-
-        # Test case 3: With stratified sampling disabled
-        expected_mean = 5.5
-        expected_var = 2.875
-        result_mean, result_var = calc_stats(strata, sample_size, n_iter, False, False)
-        self.assertAlmostEqual(result_mean, expected_mean, places=3)
-        self.assertAlmostEqual(result_var, expected_var, places=3)
-
     def test_get_table_sample_size(self):
-        mu = 0.5
-        std_1 = 1.0
-        std_2 = 1.2
-        effects = np.array([0.2, 0.5, 0.8])
-        errors = np.array([0.05, 0.1, 0.2])
+        # Needs refactor
+        mu = 10
+        std_1 = 2
+        std_2 = 2
+        effects = np.linspace(1.01, 1.1, 10)
+        errors = [0.05, 0.1, 0.15, 0.2]
 
-        expected_columns = pd.MultiIndex.from_tuples([(0.05,), (0.1,), (0.2,)], names=["errors"])
-        expected_index = pd.MultiIndex.from_tuples([("20.0%",), ("50.0%",), ("80.0%",)], names=["effects"])
-        expected_values = np.array([[81, 31, 14], [32, 12, 6], [13, 5, 3]])
-        expected_df = pd.DataFrame(expected_values, index=expected_index, columns=expected_columns)
+        expected_columns = pd.MultiIndex.from_tuples([(0.05,), (0.1,), (0.15,), (0.2,)], names=["errors"])
+        expected_index = pd.MultiIndex.from_tuples([("1.0%",), ("2.0%",), ("3.0%",), ("4.0%",), ("5.0%",),
+                                                    ("6.0%",), ("7.0%",), ("8.0%",), ("9.0%",), ("10.0%",)],
+                                                   names=["effects"])
+        expected_values = np.array([[10396, 6852, 4905, 3607], [2599, 1713, 1227, 902], [1156, 762, 545, 401],
+                                    [650, 429, 307, 226], [416, 275, 197, 145], [289, 191, 137, 101],
+                                    [213, 140, 101, 74], [163, 108, 77, 57], [129, 85, 61, 45], [104, 69, 50, 37]])
+        expected_df = pd.DataFrame(expected_values, index=expected_index, columns=expected_columns, )
 
         result_df = get_table_sample_size(mu, std_1, std_2, effects, errors)
-
         self.assertIsInstance(result_df, pd.DataFrame)
         self.assertEqual(result_df.shape, expected_df.shape)
         pd.testing.assert_frame_equal(result_df, expected_df)
+
+
+if __name__ == '__main__':
+    unittest.main()
